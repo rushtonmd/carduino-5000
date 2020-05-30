c656295639d77a4ebb270c0b425f998d0408fad9
Mark D Rushton <rushtonmd@github.com>
2020-05-30 15:45:15 -0700

    add outputs to tcu

--- a340/a340-transmission-controller.ino
+++ a340/a340-transmission-controller.ino
@@ -1,5 +1,12 @@
-//int d13pin = 10;  // LED connected to digital pin 13
-//int d13value = 0;      // variable to store the read value
+int d9pin = 9;
+int d9value = 0;
+int d12pin = 12; 
+int d12value = 0;
+int d11pin = 11; 
+int d11alue = 0;
+int d10pin = 10; 
+int d10value = 0;
+
 int a5pin = A5; 
 int a5value = 0;
 int a4pin = A4; 
@@ -17,7 +24,10 @@ int a0value = 0;
 
 void setup() {
   Serial.begin(115200);
-  //pinMode(d13pin, INPUT);
+  pinMode(d9pin, OUTPUT);
+  pinMode(d12pin, OUTPUT);
+  pinMode(d11pin, OUTPUT);
+  pinMode(d10pin, OUTPUT);
   //digitalWrite(d13value,0); 
   pinMode(a5pin, INPUT);
   digitalWrite(a5pin,0); 
@@ -40,6 +50,53 @@ void readAllPinValues(){
   a0value = digitalRead(a0pin);
 }
 
+void setTransmissionControllerOutputs() {
+
+  /// INPUTS
+  // A5 = Park
+  // A4 = Reverse
+  // A3 = Neutral
+  // A2 = Drive
+  // A1 = Low
+  // A0 = Overdrive
+
+  // OUTPUTS
+  // D12 = A
+  // D11 = B
+  // D10 = C
+
+  if (A5 == 1) { // Park
+    digitalWrite(d12pin, 0); // A
+    digitalWrite(d11pin, 1); // B
+    digitalWrite(d10pin, 0); // C
+  }
+  else if (A4 == 1) { // Reverse
+    digitalWrite(d12pin, 1); // A
+    digitalWrite(d11pin, 1); // B
+    digitalWrite(d10pin, 0); // C
+  }
+  else if (A3 == 1) { // Neutral
+    digitalWrite(d12pin, 0); // A
+    digitalWrite(d11pin, 1); // B
+    digitalWrite(d10pin, 0); // C
+  }
+  else if (A2 == 1) { // Drive
+    digitalWrite(d12pin, 0); // A
+    digitalWrite(d11pin, 1); // B
+    digitalWrite(d10pin, 1); // C
+  }
+  else if (A1 == 1) { // L
+    digitalWrite(d12pin, 1); // A
+    digitalWrite(d11pin, 0); // B
+    digitalWrite(d10pin, 0); // C
+  }
+  else {
+    digitalWrite(d12pin, 0); // A
+    digitalWrite(d11pin, 0); // B
+    digitalWrite(d10pin, 0); // C
+  }
+}
+
 void printAllInputValues() {
   Serial.print("A5:");
   Serial.print(a5value);
@@ -59,6 +116,10 @@ void printAllInputValues() {
   Serial.print("A0:");
   Serial.print(a0value);
   Serial.println();
+
+  Serial.print("D9:");
+  Serial.print(d9value);
+  Serial.println();
 }
 
 void loop() {
@@ -67,6 +128,6 @@ void loop() {
   readAllPinValues();
   
   //val = digitalRead(d13pin);   // read the input pin
-  //digitalWrite(ledPin, val);  // sets the LED to the button's value
+  digitalWrite(d9pin, a5value);  // sets the LED to the button's value
   delay(10);
 }
