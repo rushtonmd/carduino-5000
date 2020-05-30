5bbe13bf917a603ad622741694620b01002d67df
Mark D Rushton <rushtonmd@github.com>
2020-05-30 15:14:50 -0700

    inputs and prints

--- a340/a340-transmission-controller.ino
+++ a340/a340-transmission-controller.ino
@@ -1,5 +1,5 @@
-int d13pin = 10;  // LED connected to digital pin 13
-int d13value = 0;      // variable to store the read value
+//int d13pin = 10;  // LED connected to digital pin 13
+//int d13value = 0;      // variable to store the read value
 int a5pin = A5; 
 int a5value = 0;
 int a4pin = A4; 
@@ -17,8 +17,8 @@ int a0value = 0;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(d13pin, INPUT);
-  digitalWrite(d13value,0); 
+  //pinMode(d13pin, INPUT);
+  //digitalWrite(d13value,0); 
   pinMode(a5pin, INPUT);
   digitalWrite(a5pin,0); 
   pinMode(a0pin, INPUT);
@@ -28,19 +28,44 @@ void setup() {
 
 }
 
-void loop() {
-  Serial.print("D13: ");
-  Serial.println(d13value);
-  Serial.print("A5: ");
-  Serial.println(a5value);
-  Serial.print("A0: ");
-  Serial.println(a0value);
-  Serial.print("A4: ");
-  Serial.println(a4value);
-  d13value = digitalRead(d13pin);
+void readAllPinValues(){
+
+  // Read all analog pins. The 6 analog pins are mapped to the 
+  // shifter switch on the a340e transmission
   a5value = digitalRead(a5pin);
   a4value = digitalRead(a4pin);
+  a3value = digitalRead(a3pin);
+  a2value = digitalRead(a2pin);
+  a1value = digitalRead(a1pin);
   a0value = digitalRead(a0pin);
+}
+
+void printAllInputValues() {
+  Serial.print("A5:");
+  Serial.print(a5value);
+  Serial.print(" - ");
+  Serial.print("A4:");
+  Serial.print(a4value);
+  Serial.print(" - ");
+  Serial.print("A3:");
+  Serial.print(a3value);
+  Serial.print(" - ");
+  Serial.print("A2:");
+  Serial.print(a2value);
+  Serial.print(" - ");
+  Serial.print("A1:");
+  Serial.print(a1value);
+  Serial.print(" - ");
+  Serial.print("A0:");
+  Serial.print(a0value);
+  Serial.println();
+}
+
+void loop() {
+
+  printAllInputValues();
+  readAllPinValues();
+  
   //val = digitalRead(d13pin);   // read the input pin
   //digitalWrite(ledPin, val);  // sets the LED to the button's value
   delay(10);
